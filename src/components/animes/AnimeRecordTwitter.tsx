ca0d9e10ae86150311c7a62049026aebb9e19870
mhkkr <mokuhen@gmail.com>
2023-06-11 11:06:13 +0900

    fix url誤り

--- src/components/animes/AnimeRecordTwitter.tsx
+++ src/components/animes/AnimeRecordTwitter.tsx
@@ -16,7 +16,7 @@ function lengthMax140(comment: string, base: string, url: string) {
 }
 
 export default function Edit({ record, episode, user }: { record: Record, episode: Episode, user: ViewerUserQuery }) {
-  const url = `https://annict.com/works/${episode.work.annictId}/${episode.annictId}`;
+  const url = `https://annict.com/works/${episode.work.annictId}/episodes/${episode.annictId}`;
   // const url = `https://annict.com/@${user.viewer?.username}/records/${record.annictId}`;
   const title = record.comment ?
     lengthMax140(record.comment, `#${episode.work.twitterHashtag} ${episode.numberText}\n\n`, url) :
