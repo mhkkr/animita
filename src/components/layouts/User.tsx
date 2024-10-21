77f006e3d7b0556f4fee164dc26c2b24c79f9c20
kakera <mokuhen@gmail.com>
2024-10-21 13:05:02 +0900

    fix PopoverPanel PopoverButton

--- src/components/layouts/User.tsx
+++ src/components/layouts/User.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Popover, Transition } from '@headlessui/react';
+import { Popover, PopoverPanel, PopoverButton, Transition } from '@headlessui/react';
 import { signOut, useSession } from 'next-auth/react';
 
 import Icons from '~/components/icons/Icons';
@@ -18,14 +18,14 @@ export default function User() {
         leaveFrom="transform scale-100 opacity-100"
         leaveTo="transform scale-95 opacity-0"
       >
-        <Popover.Panel className="absolute top-10 sm:top-auto sm:bottom-full right-0 sm:left-4 sm:right-4 border dark:border-white/25 bg-white dark:bg-black rounded-xl overflow-hidden shadow-xl whitespace-nowrap">
+        <PopoverPanel className="absolute top-10 sm:top-auto sm:bottom-full right-0 sm:left-4 sm:right-4 border dark:border-white/25 bg-white dark:bg-black rounded-xl overflow-hidden shadow-xl whitespace-nowrap">
           <button onClick={() => signOut()} className="flex items-center p-3 w-full hover:text-red-500 font-bold" type="button">
             <Icons id="logout" type="form" className="flex-shrink-0 mr-1.5 text-lg" />
             ログアウト
           </button>
-        </Popover.Panel>
+        </PopoverPanel>
       </Transition>
-      <Popover.Button className="group/user flex items-center gap-3 sm:p-3 w-full outline-none">
+      <PopoverButton className="group/user flex items-center gap-3 sm:p-3 w-full outline-none">
         <span className="
           order-last sm:order-none
           dark:bg-white rounded-full overflow-hidden w-8 h-8 sm:w-12 sm:h-12
@@ -34,7 +34,7 @@ export default function User() {
           <img className="object-cover w-full h-full" src={data?.user?.image || ''} alt={`${data?.user?.name}さんのアイコン`} />
         </span>
         <span>{data?.user?.name}</span>
-      </Popover.Button>
+      </PopoverButton>
     </Popover>
   );
 }
